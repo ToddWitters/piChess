590203a510577ee7b1c1aaa50a33379b163e77f3
Todd Witters <roastmybeans@gmail.com>
2018-01-24 22:35:34 -0500

    Update to Stockfish 8.0

--- st_splashScreen.c
+++ st_splashScreen.c
@@ -12,8 +12,8 @@ void splashScreenEntry( event_t ev)
    displayWriteLine( 0, "piChess version 1.0", TRUE );
    displayWriteLine( 1, "by Todd Witters", TRUE );
    displayWriteLine( 2, "powered by", TRUE );
-   displayWriteLine( 3, "Stockfish 7", TRUE );
-   
+   displayWriteLine( 3, "Stockfish 8", TRUE );
+
    // Start time-out for splash screen.
    timerStart(TMR_UI_TIMEOUT, 5000, 0, EV_GOTO_MAIN_MENU);
 
