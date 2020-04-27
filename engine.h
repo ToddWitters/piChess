3c1a941a025173631d1d4a1f25fa221a1b4d6399
Todd Witters <roastmybeans@gmail.com>
2020-04-26 22:15:23 -0400

    Added missing file

---
+++ engine.h
@@ -0,0 +1,18 @@
+#ifndef _ENGINE_H_
+#define _ENGINE_H_
+
+
+#define MIN_STRENGTH  0
+#define MAX_STRENGTH 20
+
+// For untimed games, computer will search until either a fixed depth or fixed time set by user
+
+#define MIN_PLY_DEPTH      4
+#define DEFAULT_PLY_DEPTH 12
+#define MAX_PLY_DEPTH     30
+
+#define MIN_TIME_SEARCH_SEC       1
+#define DEFAULT_TIME_SEARCH_SEC   5
+#define MAX_TIME_SEARCH_SEC     120
+
+#endif
